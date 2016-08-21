5f4278d75c43d7f68e47cae7649060a01aba5f95
Laercio Santana <laerciosantana021@hotmail.com>
2016-08-21 15:08:29 -0300

    implements route chooser

--- index.php
+++ index.php
@@ -3,31 +3,46 @@
     <meta charset="utf-8"/>
     <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1" />
 
+
+    <link rel="stylesheet" type="text/css" href="./node_modules/bootstrap/dist/css/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css" href="./src/css/map.css" />
     <link rel="stylesheet" type="text/css" href="./src/css/list.css" />
   </head>
   <body>
     <div id="map"></div>
     <div id="panel">
-      <div id="route">
+      <div id="route" class="clickable-item">
         <h3 id="route-name" class="center-text">UFC</h3>
         <p id="route-description">
         </p>
-        
       </div>
       <div id="messages-title" class="center-text"><b>Mensagens</b></div>
       <div id="messages">
         <ul class="list">
-          <li class="hidden">
-            <h3 class="title">Jonny Stromberg</h3>
-            <p class="message">1986</p>
-            <p class="date">dklsajd</p>
-          </li>
         </ul>
       </div>
     </div>
+
+    <div id="select-route-modal"class="modal fade" tabindex="-1" role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title">Rotas</h4>
+          </div>
+          <div class="modal-body scrollable-y">
+            <div id="routes">
+              <ul class="list">
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <script type="text/javascript" src="./node_modules/polyline/src/polyline.js"></script>
     <script type="text/javascript" src="./node_modules/jquery/dist/jquery.min.js"></script>
+    <script type="text/javascript" src="./node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="./node_modules/list.js/dist/list.min.js"></script>
     <script type="text/javascript" src="./src/js/map.js">
     </script>
--- src/js/map.js
+++ src/js/map.js
@@ -1,71 +1,126 @@
 var SERVER_PREFIX = "http://santana.azurewebsites.net/BusTrackerSecureAPI/index.php";
+var ROUTE = 86;
+var UPDATE_TIME_POSITION = 1000;
 var map;
+var route;
+var timerId;
+var panel;
 function initMap() {
 	map = new google.maps.Map($("#map")[0], {
-	  center: {lat: -3.693760, lng: -40.355086},
-	  zoom: 16
+	  center: {lat: -3.683868, lng: -40.350334},
+	  zoom: 15
 	});
 
 	jQuery.support.cors = true;
-	$.ajax( {
-		type: "GET",
-		url: SERVER_PREFIX+"/routes/86",
-		dataType: 'json',
-		headers: {'Content-Type': 'text/plain', 'Token': 'JpURojQBMP7fD5gYC6t26jb9A40FPae2JNjRBzJpo4NoBgpkRKOW6U8b8naLPa+dEvR0Z0+tHcHEnp8Wxjj4MGfVYarqY73d2j9cF5cCAuEpus2Oj9bmuCVQjbxF7wPIViWRi99yO0YZOGF4EEpaZGRiTJCMaAV+CcyHa15soT5AY+qOJgHEsqK2irem9TYuPZP0DKipQK0sWYNoDGyszYPo0x71W8H7uVT69GFzgJQ=' },
-	})
-     .done(function(data) {
-     	//draw route from response
-        var pathEncoded = data.googleRoute.routes[0].overview_polyline.points;
+	
+	panel = new Panel({
+		el: "messages",
+		route: route
+	});
+
+	selecteRoute(ROUTE);
+}
+function selecteRoute(id){
+	if(route)
+		route.clearDrawing();
+	
+	var onCreate = function(){
+		if(timerId)
+			clearInterval(timerId);
+
+		timerId = setInterval(function(){
+			if(route.update)
+				route.update(false);
+		}, 3000);
+
+		panel.setRoute(route);
+	}
+
+	route = new Route({
+		id_route: id,
+		map: map
+	}, onCreate);
+
+	
+}
+function Route(op, onCreate){
+	op = op || {};
+
+	if(op.map === undefined)
+		throw new {message:"map not defined"};
+
+	this.map = op.map;
+
+	if(!op.id_route)
+		throw new {message:"id_buses not defined"};
+	this.id_route = op.id_route || 0;
+
+	this.routeAPI;
+	this.__getRouteFromApi(function(){
+		this.__drawRoute();
+		this.__initBuses();
+		this.update();
+		if(typeof onCreate === 'function')
+			onCreate(this);
+	}.bind(this));
+	
+}
+Route.prototype = {
+	__getRouteFromApi: function(callback){
+		$.ajax( {
+			type: "GET",
+			url: SERVER_PREFIX+"/routes/"+this.id_route,
+			dataType: 'json',
+			headers: {'Content-Type': 'text/plain', 'Token': 'JpURojQBMP7fD5gYC6t26jb9A40FPae2JNjRBzJpo4NoBgpkRKOW6U8b8naLPa+dEvR0Z0+tHcHEnp8Wxjj4MGfVYarqY73d2j9cF5cCAuEpus2Oj9bmuCVQjbxF7wPIViWRi99yO0YZOGF4EEpaZGRiTJCMaAV+CcyHa15soT5AY+qOJgHEsqK2irem9TYuPZP0DKipQK0sWYNoDGyszYPo0x71W8H7uVT69GFzgJQ=' },
+		})
+	     .done(function(data) {
+	     	this.routeAPI = data;
+	     	if(typeof callback === 'function')
+	     		callback(this);
+	     	if(typeof this.onUpdateRouteAPI === 'function')
+	     		this.onUpdateRouteAPI(this);
+		 }.bind(this))
+		 .fail(function(data) {
+		    console.log("error:\n", data);
+		 });
+	},
+	__initBuses: function(){
+		//Make buses
+		var busesId = this.routeAPI.id_buses;
+		this.buses = [];
+		for(var i = 0; i < busesId.length; i++){
+			this.buses[i] = new Bus({map: this.map, id_route: this.id_route, id_buses:busesId[i]});
+		}
+	},
+	__drawRoute: function(){
+		//draw route from response
+        var pathEncoded = this.routeAPI.googleRoute.routes[0].overview_polyline.points;
         var path = google.maps.geometry.encoding.decodePath(pathEncoded);
-        var poly = new google.maps.Polyline({
+        this.poly = new google.maps.Polyline({
 		    strokeColor: '#000000',
 		    strokeOpacity: 0.7,
 		    strokeWeight: 5,
 		    path: path
 		});
-		poly.setMap(map);
-
-		//Make buses
-		var busesId = data.id_buses;
-		var buses = [];
-		for(var i = 0; i < busesId.length; i++){
-			console.log(busesId[i]);
-			buses[i] = new Bus({map: map, id_route: 86, id_buses:busesId[i]});
-		}
+		this.poly.setMap(this.map);
+	},
+	update: function(data){
+		if(data)
+			this.__getRouteFromApi();
 
 		//Draw markes on map
-		var updatePosition = function(){
-			for(var i = 0; i < buses.length; i++)
-				buses[i].updatePosition();
-		};
+		if(this.buses instanceof Array)
+			for(var i = 0; i < this.buses.length; i++)
+				this.buses[i].updatePosition();
+	},
+	clearDrawing: function(){
+		if(this.buses instanceof Array)
+			for(var i = 0; i < this.buses.length; i++)
+				this.buses[i].clearDrawing();
 
-		setInterval(updatePosition, 3000);
-
-		//update route in panel
-		$("#route-name").text(data.name);
-	 	$("#route-description").text(data.description);
-	 })
-	 .fail(function(data) {
-	    console.log("error:\n", data);
-	 });
-
-	 //request messages
- 	$.ajax( {
-		type: "GET",
-		url: SERVER_PREFIX+"/routes/86/messages",
-		dataType: 'json',
-		headers: {'Content-Type': 'text/plain', 'Token': 'JpURojQBMP7fD5gYC6t26jb9A40FPae2JNjRBzJpo4NoBgpkRKOW6U8b8naLPa+dEvR0Z0+tHcHEnp8Wxjj4MGfVYarqY73d2j9cF5cCAuEpus2Oj9bmuCVQjbxF7wPIViWRi99yO0YZOGF4EEpaZGRiTJCMaAV+CcyHa15soT5AY+qOJgHEsqK2irem9TYuPZP0DKipQK0sWYNoDGyszYPo0x71W8H7uVT69GFzgJQ=' },
-	})
-     .done(function(data) {
-     	console.log(data);
-     	var panel = new Panel({
-     		el:"messages",
-     		messages: data
-     	});
-	 })
-	 .fail(function(data) {
-	    console.log("error:\n", data);
-	 });
+		this.poly.setMap(null);
+	},
+	onUpdateRouteAPI: function(){}
 }
 
 function Bus(op){
@@ -99,6 +154,9 @@ Bus.prototype = {
 		 .fail(function(data) {
 		    console.log("error:\n", data);
 		 });
+	},
+	clearDrawing: function(){
+		this.marker.setMap(null);
 	}
 }
 
@@ -107,17 +165,83 @@ function Panel(op){
 
 	this.$el = $(op.el);
 	this.el = op.el;
-	this.messages = op.messages || [];
-	this.init();
+	this.messages;
+	this.messageList = {};
+	
+	this.routesEl = 'routes';
+	this.route = op.route || {};
+	this.routeList = {};
+	
+
+
+	this.update(this.init.bind(this));
 }
 Panel.prototype = {
 	init: function(){
+		this.__updateMessagesList;
+		this.setRoute(this.route);
+		
+		if(!this.routes)
+			this.__makeRoutesList();
+
+		$("#route").on("click", function(){
+			$("#select-route-modal").modal("show");	
+		});
+
+	},
+	__makeRoutesList: function(){
+		$.ajax( {
+			type: "GET",
+			url: SERVER_PREFIX+"/routes",
+			dataType: 'json',
+			headers: {'Content-Type': 'text/plain', 'Token': 'JpURojQBMP7fD5gYC6t26jb9A40FPae2JNjRBzJpo4NoBgpkRKOW6U8b8naLPa+dEvR0Z0+tHcHEnp8Wxjj4MGfVYarqY73d2j9cF5cCAuEpus2Oj9bmuCVQjbxF7wPIViWRi99yO0YZOGF4EEpaZGRiTJCMaAV+CcyHa15soT5AY+qOJgHEsqK2irem9TYuPZP0DKipQK0sWYNoDGyszYPo0x71W8H7uVT69GFzgJQ=' },
+		})
+	     .done(function(routes) {
+	     	console.log(routes);
+	     	this.routes = routes;
+	     	var options = {
+			  valueNames: [ 'title', 'description', 'id_route'],
+			  item: '<li class="clickable-item">'+
+			            '<h3 class="title"></h3>'+
+			            '<p class="description"></p>'+
+			        '</li>'
+			};
+
+			var values = [];
+			for(var i = 0; i < this.routes.length; i++){
+				values[i] = {
+					title: this.routes[i].name,
+					description: this.routes[i].description,
+					id_route: this.routes[i].id_routes
+				};
+			}
+
+			if(typeof this.routeList.clear === 'function')
+				this.routeList.clear();
+			this.routeList = new List(this.routesEl, options, values);
+
+			var items = this.routeList.items;
+			for(var i = 0; i < items.length; i++){
+
+				$(items[i].elm).on("click", function(){
+					console.log(items[this.index].values().id_route);
+					selecteRoute(items[this.index].values().id_route);
+					$("#select-route-modal").modal("hide");
+				}.bind({index: i+0}));
+			}
+
+		 }.bind(this))
+		 .fail(function(data) {
+		    console.log("error:\n", data);
+		 });
+	},
+	__updateMessagesList: function(){
 		var options = {
 		  valueNames: [ 'title', 'message', 'date' ],
 		  item: '<li>'+
-		            '<h3 class="title">Jonny Stromberg</h3>'+
-		            '<p class="message">1986</p>'+
-		            '<p class="date">dklsajd</p>'+
+		            '<h3 class="title"></h3>'+
+		            '<p class="message"></p>'+
+		            '<p class="date"></p>'+
 		        '</li>'
 		};
 
@@ -130,6 +254,42 @@ Panel.prototype = {
 			};
 		}
 
-		var messageList = new List(this.el, options, values);
+		if(typeof this.messageList.clear === 'function')
+			this.messageList.clear();
+		this.messageList = new List(this.el, options, values);
+	},
+	update: function(callback){
+		if(this.route.id_route){
+			//request messages
+		 	$.ajax( {
+				type: "GET",
+				url: SERVER_PREFIX+"/routes/"+this.route.id_route+"/messages?buses=true",
+				dataType: 'json',
+				headers: {'Content-Type': 'text/plain', 'Token': 'JpURojQBMP7fD5gYC6t26jb9A40FPae2JNjRBzJpo4NoBgpkRKOW6U8b8naLPa+dEvR0Z0+tHcHEnp8Wxjj4MGfVYarqY73d2j9cF5cCAuEpus2Oj9bmuCVQjbxF7wPIViWRi99yO0YZOGF4EEpaZGRiTJCMaAV+CcyHa15soT5AY+qOJgHEsqK2irem9TYuPZP0DKipQK0sWYNoDGyszYPo0x71W8H7uVT69GFzgJQ=' },
+			})
+		     .done(function(data) {
+		     	this.messages = data;
+		     	this.__updateMessagesList();
+		     	if(typeof callback === 'function')
+		     		callback(this);
+			 }.bind(this))
+			 .fail(function(data) {
+			    console.log("error:\n", data);
+			 });
+
+			 this.__updateInfoRoute();
+		}
+	},
+	__updateInfoRoute: function(){
+		//update route in panel
+		if(this.route.routeAPI){
+			$("#route-name").text(this.route.routeAPI.name);
+			$("#route-description").text(this.route.routeAPI.description);
+		}
+	},
+	setRoute: function(route){
+		this.route = route;
+		this.update(this.init.bind(this));
+		this.route.onUpdateRouteAPI = this.__updateInfoRoute.bind(this);
 	}
 }
