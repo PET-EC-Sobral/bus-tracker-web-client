0e1033d362ffdcdae0ffee890944b5ce08e8af74
Laercio Santana <laerciosantana021@hotmail.com>
2016-12-01 10:04:50 -0300

    refactoring  icon url

--- src/js/map.js
+++ src/js/map.js
@@ -3,7 +3,8 @@ var ROUTE = 86;
 var UPDATE_TIME_POSITION = 3000;
 var UPDATE_TIME_MESSAGE = 5000;
 var TOKEN = "JpURojQBMP7fD5gYC6t26jb9A40FPae2JNjRBzJpo4NoBgpkRKOW6U8b8naLPa+dEvR0Z0+tHcHEnp8Wxjj4MGfVYarqY73d2j9cF5cCAuEpus2Oj9bmuCVQjbxF7wPIViWRi99yO0YZOGF4EEpaZGRiTJCMaAV+CcyHa15soT5AY+qOJgHEsqK2irem9TYuPZP0DKipQK0sWYNoDGyszYPo0x71W8H7uVT69GFzgJQ=";
-var MARKER_ICON = "assets/img/marker.png"
+var MARKER_ICON = "assets/img/marker.png";
+var MARKER_ICON_STOP = "assets/img/marker-stop.png";
 var map;
 var route;
 var timerId;
@@ -304,9 +305,9 @@ Bus.prototype = {
 	},
 	setIcon: function(icon){
 		if(icon == 'stop')
-			this.marker.icon.url = "assets/img/marker-stop.png";
+			this.marker.icon.url = MARKER_ICON_STOP;
 		else
-			this.marker.icon.url = "assets/img/marker.png";
+			this.marker.icon.url = MARKER_ICON;
 	},
 	isStoped: function(){
 		var last = Date.parse(this.lastUpdate);
